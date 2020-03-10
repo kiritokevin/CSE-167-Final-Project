fc0abe8799156bcb0ab1e814e0770772d5b6eb55
Yilin Cai <yilincai@YilindeMacBook-Pro.local>
2020-03-10 15:20:53 -0700

    Add Rec Class

---
+++ CSE 167 FInal Project/CSE 167 FInal Project/Rec.h
@@ -0,0 +1,34 @@
+#ifndef _CUBE_H_
+#define _CUBE_H_
+
+#ifdef __APPLE__
+#include <OpenGL/gl3.h>
+#else
+#include <GL/glew.h>
+#endif
+
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+
+#include "Object.h"
+
+class Rec
+{
+private:
+    glm::mat4 model;
+    GLuint vao;
+    GLuint vbos[2];
+public:
+    Rec(float size, glm::vec3 min, glm::vec3 max);
+    ~Rec();
+
+    void draw(GLuint shaderProgram, glm::mat4 View, glm::mat4 Projection);
+    void update(int num);
+    void spin(float deg);
+    void move(int direction);
+};
+
+#endif
